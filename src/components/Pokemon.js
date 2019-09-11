01777e6a57ffcb78949d4ff34f348a020cbc4a4e
anamontiaga <amontiaga@gmail.com>
2019-09-11 19:02:57 +0200

    finish exercise

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,11 +4,13 @@ import "../index.css";
 const Pokemon = props => {
   return (
     <li className="pokemon">
-      <img src={props.url} alt={props.name} />
-      <h2>{props.name}</h2>
-      {props.types.map(type => {
-        return <button>{type}</button>;
-      })}
+      <img className="pokemon--img" src={props.url} alt={props.name} />
+      <h2 className="pokemon--title">{props.name}</h2>
+      <div className="pokemon--btns">
+        {props.types.map(type => {
+          return <button className="pokemon--btn">{type}</button>;
+        })}
+      </div>
     </li>
   );
 };
