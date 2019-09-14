370c70a912277d90e304f48cc6992db2d2d826f3
anamontiaga <amontiaga@gmail.com>
2019-09-14 17:14:08 +0200

    move li aout of Pokemon dummy component

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "../stylesheets/scss/Pokemon.scss";
 
+const pokemonTypes = props => {
+  return props.types.map(type => {
+    return <li className="pokemon--type">{type}</li>;
+  });
+};
+
 const Pokemon = props => {
   return (
     <li className="pokemon">
       <img className="pokemon--img" src={props.url} alt={props.name} />
       <h2 className="pokemon--title">{props.name}</h2>
       <div className="pokemon--btns">
-        {props.types.map(type => {
-          return <button className="pokemon--btn">{type}</button>;
-        })}
+        <ul className="pokemon--types--list">{pokemonTypes(props)}</ul>
       </div>
     </li>
   );
